59b67ddf81c610303f68188f4aec5ae73f3b3ffc
Brett Sutton <bsutton@noojee.com.au>
2021-11-17 14:47:44 +1100

    re packed the rules.yaml

--- lib/src/dcli/resources/generated/resource_registry.g.dart
+++ lib/src/dcli/resources/generated/resource_registry.g.dart
@@ -1,16 +1,17 @@
 // ignore: prefer_relative_imports
 import 'package:dcli/dcli.dart';
-import 'Ceeebfccdfedc.g.dart';
+import 'Afaaaeaaca.g.dart';
 
 /// GENERATED -- GENERATED
-///
+/// 
 /// DO NOT MODIFIY
-///
+/// 
 /// This script is generated via [Resource.pack()].
-///
+/// 
 /// GENERATED - GENERATED
 
 class ResourceRegistry {
+
   /// Map of the packed files.
   /// Use the path of a packed file (relative to the resource directory)
   /// to access the packed resource and then call [PackedResource].unpack()
@@ -19,6 +20,7 @@ class ResourceRegistry {
   /// ResourceRegistry.resources['rules.yaml'].unpack(join(HOME, '.mysettings', 'rules.yaml'));
   /// ```
   static const Map<String, PackedResource> resources = {
-    'rules.yaml': Ceeebfccdfedc(),
-  };
-}
+      'rules.yaml' : Afaaaeaaca(),
+          };
+  }
+  
