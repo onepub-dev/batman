c53d7c06c89ed4edccee1011ec7a247e1d64df19
Brett Sutton <bsutton@noojee.com.au>
2021-11-19 11:23:49 +1100

    removed hardcoded verbose logging.

--- bin/pcifim.dart
+++ bin/pcifim.dart
@@ -1,9 +1,7 @@
 #! /usr/bin/env dcli
 
-import 'package:dcli/dcli.dart' hide run;
 import 'package:pci_file_monitor/src/entry_point.dart';
 
 void main(List<String> arguments) {
-  Settings().setVerbose(enabled: true);
   run(arguments);
 }
